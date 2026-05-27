0bb31bce231acda20b47e2143e54156c7b8ae897
wcs2023 <2023cs164@stu.ucsc.cmb.ac.lk>
2026-05-27 12:01:53 +0530

    update database configuration for production environment and adjust base URLs

--- app/core/config.php
+++ app/core/config.php
@@ -6,21 +6,21 @@ if (!isset($_SERVER['SERVER_NAME'])) {
 }
 
 if($_SERVER['SERVER_NAME'] == 'localhost') {
-    define('DBHOST', getenv('DB_HOST') ?: 'localhost');
-    define('DBNAME', getenv('DB_NAME') ?: 'universe_db');
-    define('DBUSER', getenv('DB_USER') ?: 'root');
-    define('DBPASS', getenv('DB_PASS') ?: '');
-    define('DBDRIVER', getenv('DB_DRIVER') ?: '');
+    define('DBHOST', getenv('DB_HOST') ?: 'sql303.infinityfree.com');
+    define('DBNAME', getenv('DB_NAME') ?: 'if0_42029602_universe');
+    define('DBUSER', getenv('DB_USER') ?: 'if0_42029602');
+    define('DBPASS', getenv('DB_PASS') ?: '2RdlSJQesHA6');
+    define('DBDRIVER', getenv('DB_DRIVER') ?: 'mysql');
     define('BASE_URL', getenv('BASE_URL') ?: 'http://localhost/UniVerse/public');
 } else {
-    define('DBHOST', getenv('DB_HOST') ?: 'localhost');
-    define('DBNAME', getenv('DB_NAME') ?: 'universe_db');
-    define('DBUSER', getenv('DB_USER') ?: 'root');
-    define('DBPASS', getenv('DB_PASS') ?: '');
-    define('DBDRIVER', getenv('DB_DRIVER') ?: '');
-    define('BASE_URL', getenv('BASE_URL') ?: 'https://yourdomain.com/public');
+    define('DBHOST', getenv('DB_HOST') ?: 'sql303.infinityfree.com');
+    define('DBNAME', getenv('DB_NAME') ?: 'if0_42029602_universe');
+    define('DBUSER', getenv('DB_USER') ?: 'if0_42029602');
+    define('DBPASS', getenv('DB_PASS') ?: '2RdlSJQesHA6');
+    define('DBDRIVER', getenv('DB_DRIVER') ?: 'mysql');
+    define('BASE_URL', getenv('BASE_URL') ?: 'https://uni-verse-beige.vercel.app/public');
 }
 
 define('APPROOT',dirname(dirname(__FILE__))); 
-define('URLROOT', getenv('URL_ROOT') ?: 'http://localhost/UniVerse'); 
+define('URLROOT', getenv('URL_ROOT') ?: 'https://uni-verse-beige.vercel.app'); 
 define('SITENAME','UniVerse');
